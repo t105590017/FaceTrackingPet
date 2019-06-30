3cb978500cf44cab3e601bbd2dfa22382797fdd0
t105590017 <t105590017@ntut.org.tw>
2019-06-30 06:14:29 -0700

    PWM GPIO

--- Code/GPIOController.py
+++ Code/GPIOController.py
@@ -5,28 +5,32 @@ import pexpect
 
 gpioInfo = ConfigParser()
 gpioInfo.read("gpioConfig.ini")
-gpioInfo.sections()
+
+AutoTerminalEnable = False
 
 def AutoTerminalGetPermisssion():
+    if not AutoTerminalEnable:
+        return []
+
     child = pexpect.spawn("sudo -i")
     child.expect([":", "#", pexpect.EOF, pexpect.TIMEOUT])
     child.sendline(gpioInfo.get("UserPassword", "rootPassword"))
     return child
-    # return []
 
 def AutoTerminal(command, child = None):
+    if not AutoTerminalEnable:
+        return None
+
     if child is None:
         child = pexpect.spawn(command)
         return child
-        # return None
     
     child.expect(["#", pexpect.EOF, pexpect.TIMEOUT])
     child.sendline(command)
     return child
-    # return None
 
 def AutoTerminalClose(child = None):
-    if child is None:
+    if child is None or not AutoTerminalEnable:
         return
     child.expect("#")
     child.close(force = False)
@@ -35,8 +39,8 @@ def AutoTerminalClose(child = None):
 class NormalGPIO:
     def __init__(self):
         self.usingList = []
-        self.controlPath = "/sys/class/gpio"
-        self.gpioMap = gpioInfo["NumberOfNormalGPIO"]
+        self._controlPath = "/sys/class/gpio"
+        self._gpioMap = gpioInfo["NumberOfNormalGPIO"]
 
     def __del__(self):
         for gpio in self.usingList:
@@ -44,89 +48,192 @@ class NormalGPIO:
             self.Direction(gpio, "in")
         self.UnExport("ALL")
 
-    def Export(self, numberOfGPIO, direction = "out"):
-        if numberOfGPIO not in self.gpioMap:
-            print(Fore.RED + "[ERROR]" + Fore.RESET + " gpio number " + numberOfGPIO + " can't be use !!")
+    def Export(self, pinNumber, direction = "out"):
+        if pinNumber not in self._gpioMap:
+            print("{0}[ERROR]{1} pin number {2} can't be use !!".format(Fore.RED, Fore.RESET, pinNumber))
             return False
 
-        if numberOfGPIO.upper() in self.usingList:
-            print(Fore.RED + "[ERROR]" + Fore.RESET + " gpio number " + numberOfGPIO + " has be used !!")
+        if pinNumber.upper() in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} has be used !!".format(Fore.RED, Fore.RESET, pinNumber))
             return False
 
         child = AutoTerminalGetPermisssion()
-        AutoTerminal("echo " + self.gpioMap[numberOfGPIO] + " > " + self.controlPath + "/export", child)
-        AutoTerminal("echo " + direction + " > " + self.controlPath + "/gpio" + self.gpioMap[numberOfGPIO] + "/direction", child)        
+        AutoTerminal("echo " + self._gpioMap[pinNumber] + " > " + self._controlPath + "/export", child)
+        AutoTerminal("echo " + direction + " > " + self._controlPath + "/gpio" + self._gpioMap[pinNumber] + "/direction", child)        
         AutoTerminalClose(child)
-        print(Fore.GREEN + "[SUCCESS]" + Fore.RESET + " gpio number "+ numberOfGPIO + " export")
-        self.usingList.append(numberOfGPIO.upper())
+        print("{0}[SUCCESS]{1} pin number {2} export".format(Fore.GREEN, Fore.RESET, pinNumber))
+        self.usingList.append(pinNumber.upper())
+        return True
 
-    def UnExport(self, numberOfGPIO = "ALL"):
-        if numberOfGPIO == "ALL":
+    def UnExport(self, pinNumber = "ALL"):
+        if pinNumber == "ALL":
             child = AutoTerminalGetPermisssion()
             for gpio in self.usingList:
-                AutoTerminal("echo " + self.gpioMap[gpio] + " > " + self.controlPath + "/unexport", child)
-                print(Fore.GREEN + "[UnExport]" + Fore.RESET + " gpio number "+ gpio + " unexport")
+                AutoTerminal("echo " + self._gpioMap[gpio] + " > " + self._controlPath + "/unexport", child)
+                print("{0}[SUCCESS]{1} pin number {2} unexport".format(Fore.GREEN, Fore.RESET, pinNumber))
             
             self.usingList.clear()
             AutoTerminalClose(child)         
             return True
 
-        if numberOfGPIO.upper() not in self.usingList:
-            print(Fore.RED + "[ERROR]" + Fore.RESET +" gpio number " + numberOfGPIO + " can't be unexport !!")
+        if pinNumber.upper() not in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} can't be unexport !!".format(Fore.RED, Fore.RESET, pinNumber))
             return False
 
         child = AutoTerminalGetPermisssion()
-        AutoTerminal("echo " + self.gpioMap[numberOfGPIO] + " > " + self.controlPath + "/unexport", child)
+        AutoTerminal("echo " + self._gpioMap[pinNumber] + " > " + self._controlPath + "/unexport", child)
         AutoTerminalClose(child)
-        print(Fore.GREEN + "[SUCCESS]" + Fore.RESET + " gpio number "+ numberOfGPIO + " unexport")
-        self.usingList.remove(numberOfGPIO.upper())
+        print(Fore.GREEN + "[SUCCESS]" + Fore.RESET + " pin number "+ pinNumber + " unexport")
+        self.usingList.remove(pinNumber.upper())
         return True
 
-    def Value(self, numberOfGPIO, value):
-        if numberOfGPIO.upper() not in self.usingList:
-            print(Fore.RED + "[ERROR]" + Fore.RESET + " gpio number " + numberOfGPIO + " is not export !!")
+    def Value(self, pinNumber, value):
+        if pinNumber.upper() not in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} is not export !!".format(Fore.RED, Fore.RESET, pinNumber))
             return False
 
         child = AutoTerminalGetPermisssion()
-        AutoTerminal("echo " + value + " > " + self.controlPath + "/gpio" + self.gpioMap[numberOfGPIO] + "/value", child)
+        AutoTerminal("echo " + value + " > " + self._controlPath + "/gpio" + self._gpioMap[pinNumber] + "/value", child)
         AutoTerminalClose(child)
-        print(Fore.GREEN + "[SUCCESS]" + Fore.RESET + " gpio number "+ numberOfGPIO + " set value to " + value)
+        print("{0}[SUCCESS]{1} pin number {2} set value to {3}".format(Fore.GREEN, Fore.RESET, pinNumber, value))
         return True
 
-    def Direction(self, numberOfGPIO, direction):
-        if numberOfGPIO.upper() not in self.usingList:
-            print(Fore.RED + "[ERROR]" + Fore.RESET + " gpio number " + numberOfGPIO + " is not export !!")
+    def Direction(self, pinNumber, direction):
+        if pinNumber.upper() not in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} is not export !!".format(Fore.RED, Fore.RESET, pinNumber))
             return False
 
         child = AutoTerminalGetPermisssion()
-        AutoTerminal("echo " + direction + " > " + self.controlPath + "/gpio" + self.gpioMap[numberOfGPIO] + "/direction", child)
+        AutoTerminal("echo " + direction + " > " + self._controlPath + "/gpio" + self._gpioMap[pinNumber] + "/direction", child)
         AutoTerminalClose(child)
-        print(Fore.GREEN + "[SUCCESS]" + Fore.RESET + " gpio number "+ numberOfGPIO + " set direction to " + direction)
+        print("{0}[SUCCESS]{1} pin number {2} set direction to {3}".format(Fore.GREEN, Fore.RESET, pinNumber, direction))
         return True
         
+class PwmGPIO:
+    def __init__(self):
+        self.usingList = []
+        self._controlPath = "/sys/class/pwm"
+        self._gpioMap = gpioInfo["NumberOfPwmGPIO"]
+        self._period = -1
+
+    def __del__(self):
+        # for gpio in self.usingList:
+        #     pass
+        self.UnExport("ALL")
+
+    def Export(self, pinNumber):
+        if pinNumber not in self._gpioMap:
+            print("{0}[ERROR]{1} pin number {2} can't be use !!".format(Fore.RED, Fore.RESET, pinNumber))
+            return False
+
+        if pinNumber.upper() in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} has be used !!".format(Fore.RED, Fore.RESET, pinNumber))
+            return False
+
+        child = AutoTerminalGetPermisssion()
+        AutoTerminal("echo 0 > " + self._controlPath + "/" + self._gpioMap[pinNumber] + "/export", child)      
+        AutoTerminalClose(child)
+        print("{0}[SUCCESS]{1} pin number {2} export".format(Fore.GREEN, Fore.RESET, pinNumber))
+        self.usingList.append(pinNumber.upper())
+
+    def UnExport(self, pinNumber = "ALL"):
+        # if pinNumber == "ALL":
+        #     child = AutoTerminalGetPermisssion()
+        #     for gpio in self.usingList:
+        #         AutoTerminal("echo " + self._gpioMap[gpio] + " > " + self._controlPath + "/unexport", child)
+        #         print("{0}[SUCCESS]{1} pin number {2} unexport".format(Fore.GREEN, Fore.RESET, pinNumber))
+            
+        #     self.usingList.clear()
+        #     AutoTerminalClose(child)         
+        #     return True
+
+        # if pinNumber.upper() not in self.usingList:
+        #     print("{0}[ERROR]{1} pin number {2} can't be unexport !!".format(Fore.RED, Fore.RESET, pinNumber))
+        #     return False
+
+        # child = AutoTerminalGetPermisssion()
+        # AutoTerminal("echo " + self._gpioMap[pinNumber] + " > " + self._controlPath + "/unexport", child)
+        # AutoTerminalClose(child)
+        # print(Fore.GREEN + "[SUCCESS]" + Fore.RESET + " pin number "+ pinNumber + " unexport")
+        # self.usingList.remove(pinNumber.upper())
+        # return True
+        pass
+
+    def Period(self, pinNumber, period):
+        if pinNumber.upper() not in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} is not export !!".format(Fore.RED, Fore.RESET, pinNumber))
+            return False
+
+        child = AutoTerminalGetPermisssion()
+        AutoTerminal("echo " + str(period) + " > " + self._controlPath + "/" + self._gpioMap[pinNumber] + "/pwm0/period", child)      
+        AutoTerminalClose(child)
+        self._period = period
+        print("{0}[SUCCESS]{1} pin number {2} set period to {3}".format(Fore.GREEN, Fore.RESET, pinNumber, period))
+        return True
+
+    def DutyCycle(self, pinNumber, dutyCycle):
+        if pinNumber.upper() not in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} is not export !!".format(Fore.RED, Fore.RESET, pinNumber))
+            return False
+
+        if self._period is -1:
+            print("{0}[ERROR]{1} pin number {2} need set period first !!".format(Fore.RED, Fore.RESET, pinNumber))
+            return False
+
+        child = AutoTerminalGetPermisssion()
+        AutoTerminal("echo " + str(int(self._period * dutyCycle)) + " > " + self._controlPath + "/" + self._gpioMap[pinNumber] + "/pwm0/duty_cycle", child)      
+        AutoTerminalClose(child)
+        print("{0}[SUCCESS]{1} pin number {2} set dutyCycle to {3}".format(Fore.GREEN, Fore.RESET, pinNumber, int(self._period * dutyCycle)))
+        return True
+
+    def Enable(self, pinNumber, enable = True):
+        if pinNumber.upper() not in self.usingList:
+            print("{0}[ERROR]{1} pin number {2} is not export !!".format(Fore.RED, Fore.RESET, pinNumber))
+            return False
+
+        child = AutoTerminalGetPermisssion()
+        AutoTerminal("echo " + str(1 if enable else 0) + " > " + self._controlPath + "/" + self._gpioMap[pinNumber] + "/pwm0/enable", child)      
+        AutoTerminalClose(child)
+        print("{0}[SUCCESS]{1} pin number {2} set enable to {3}".format(Fore.GREEN, Fore.RESET, pinNumber, 1 if enable else 0))
+        return True
+
+
 
 
 if __name__ == "__main__":
     import time
 
-    n = NormalGPIO()
+    # n = NormalGPIO()
 
-    n.Export("1d3")
-    n.Export("1")
-    n.Export("12aS")
-    n.Export("linux26")
-    n.Export("linux25")
+    # n.Export("1d3")
+    # n.Export("1")
+    # n.Export("12aS")
+    # n.Export("pIn37")
+    # n.Export("linux25")
 
-    n.Value("linux26", "1")
+    # n.Value("pin37", "1")
 
-    time.sleep(2)
+    # time.sleep(2)
 
-    n.Value("linux26", "0")
+    # n.Value("pin37", "0")
 
-    time.sleep(2)
+    # time.sleep(2)
 
-    n.Value("linux26", "1")
+    # n.Value("Pin37", "1")
 
-    time.sleep(2)
+    # time.sleep(2)
 
-    print(n.usingList)
+    p = PwmGPIO()
+
+    p.Export("dsa")
+    p.Export("PIN33")
+    p.Export("pin33")
+
+    p.DutyCycle("pin33", 0.5)
+    p.Period("pin33", 3413333)
+    p.DutyCycle("pin33", 0.5)
+
+    p.Enable("pin33", True)
+    p.Enable("pin26", True)
+
+    print(p.usingList)
