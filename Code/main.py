83df53cac14274d1e2f18a909f76789457475cd4
t105590017 <t105590017@ntut.org.tw>
2019-07-06 13:39:57 +0800

    把臉部辨識從Controller分離 新增CameraMoveAction

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,15 @@
 from PetController import PetController
+from FaceDetectorAction import FaceDetectorAction
+from CameraMoveAction import CameraMoveAction
  
 if __name__ == '__main__':
     pc = PetController()
+
+    fda = FaceDetectorAction()
+    pc.AddNewAction(fda)
+
+    cma = CameraMoveAction()
+    pc.AddNewAction(cma)
+
     pc.Run()
     
